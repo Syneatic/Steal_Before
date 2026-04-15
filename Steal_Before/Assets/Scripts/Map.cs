77f9b8d75ee33ce1a41cba3c54745d91eb777dfc
WenJie05 <Echospeed@outlook.com>
2026-04-15 18:20:32 +0800

    Update map.cs
    Update will allow you to edit the map, without breaking boundary of the specified map.

--- Steal_Before/Assets/Scripts/Map.cs
+++ Steal_Before/Assets/Scripts/Map.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum FloorType 
+public enum FloorType
 {
-    EdgeWall,
-    VerticalEdgeWall,
-    HorizontalEdgeWall,
-    Space,
+    BaseCornerWall,             // Base Corner Wall     #DON'T MOVE
+    VerticalWall,               // Vertical Wall        #DON'T MOVE
+    BaseHorizontalWall,         // Base Horizontal Wall #DON'T MOVE
+    Tile,                       // Tile                 #DON'T MOVE
+    TopCornerWall,              // Top Corner Wall
+    VerticleWallMiddle,         // Verticle Wall (Middle)
+    MiddleWalls,                // Middle Walls 
+    PlusBlock,                  // Plus Block
+    TBlockHoriontal,            // T Block Hori
+    TBlockVerti,                // T Block Verti
+    TopHorizontalWall,          // Top HWall
     Obstacle
 }
 
+// 1. This tag forces the script to run in the Editor without pressing Play
+[ExecuteAlways]
 public class Map : MonoBehaviour
 {
     // Public classes
@@ -21,13 +30,31 @@ public class Map : MonoBehaviour
     public Transform[] objstaclePos;
 
     // Private classes
-    FloorType [,] mapGrid;
+    FloorType[,] mapGrid;
 
-    // Start is called before the first frame update
-    void Start()
+    // We use these to check if the user changed the size in the Inspector
+    private int lastWidth;
+    private int lastHeight;
+    private Transform boundaryHolder;
+
+    // 2. We use Update to detect changes in Edit Mode
+    void Update()
     {
-        GenerateMapData();
-        DrawMap();
+        // Only run this logic if we are NOT in Play Mode
+        if (!Application.isPlaying)
+        {
+            // Only redraw the map IF the width or height was actually changed.
+            // (Without this check, Unity would try to redraw the map 60 times a second and crash!)
+            if (width != lastWidth || height != lastHeight)
+            {
+                GenerateMapData();
+                DrawMap();
+
+                // Save the new dimensions so we don't redraw again until they change
+                lastWidth = width;
+                lastHeight = height;
+            }
+        }
     }
 
     void GenerateMapData()
@@ -38,84 +65,99 @@ public class Map : MonoBehaviour
         {
             for (int j = 0; j < height; ++j)
             {
-                if((i == 0 && j == 0) || (i == width - 1 && j == height - 1) || (i == 0 && j == height - 1) || (i == width -1 && j == 0))
+                if ((i == 0 && j == 0) || (i == width - 1 && j == height - 1) || (i == 0 && j == height - 1) || (i == width - 1 && j == 0))
                 {
-                    mapGrid[i, j] = FloorType.EdgeWall; // Wall ID
+                    mapGrid[i, j] = FloorType.BaseCornerWall;
                 }
                 else if ((i == 0 && j <= height - 2) || (i == width - 1 && j <= height - 2))
                 {
-                    mapGrid[i, j] = FloorType.VerticalEdgeWall; // Wall ID
+                    mapGrid[i, j] = FloorType.VerticalWall;
                 }
                 else if ((i <= width - 1 && j == 0) || (i <= width - 1 && j == height - 1))
                 {
-                    mapGrid[i, j] = FloorType.HorizontalEdgeWall; ; // Wall ID
+                    mapGrid[i, j] = FloorType.BaseHorizontalWall;
                 }
-                else if((j > 0 && j < height - 1))
+                else if ((j > 0 && j < height - 1))
                 {
-                    mapGrid[i, j] = FloorType.Space; // Floor ID
+                    mapGrid[i, j] = FloorType.Tile;
                 }
             }
         }
-        Debug.Log("mapArray has a totoal of " + mapGrid.Length + " stored data");
+        Debug.Log("mapArray has a total of " + mapGrid.Length + " stored data");
     }
 
     void DrawMap()
     {
+        ClearOldBoundaries();
+
+        GameObject holderObj = new GameObject("GeneratedBoundaries");
+        holderObj.transform.parent = this.transform;
+        boundaryHolder = holderObj.transform;
+
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
                 FloorType tileID = mapGrid[x, y];
-
-                // Skip ID 0 if we consider it "Empty space"
-                //if (tileID == 0) continue;
-
                 Vector3 spawnPosition = new Vector3(x * tileSize, y * tileSize, 0);
-                // Check if we have a prefab assigned for this ID to avoid errors
+
                 if (((int)tileID) < tilePrefabs.Length && tilePrefabs[((int)tileID)] != null)
                 {
-                    // Calculate the world position based on the array coordinates
-                    // We use (x, 0, y) for a 3D ground map, or (x, y, 0) for a 2D game.
-                    if (tileID == FloorType.HorizontalEdgeWall && y == 0)
+                    // 1. Create a temporary variable to hold the wall we are about to spawn
+                    GameObject spawnedWall = null;
+
+                    if (tileID == FloorType.BaseHorizontalWall && y == 0)
                     {
-                        Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.identity, this.transform);
+                        spawnedWall = Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.identity, boundaryHolder);
+                        spawnedWall.hideFlags = HideFlags.NotEditable;
                     }
-                    else if(tileID == FloorType.HorizontalEdgeWall && y == height - 1)
+                    else if (tileID == FloorType.BaseHorizontalWall && y == height - 1)
                     {
-                        Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.Euler(new Vector3(180.0f, 0.0f, 0.0f)), this.transform);
+                        spawnedWall = Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.Euler(new Vector3(180.0f, 0.0f, 0.0f)), boundaryHolder);
+                        spawnedWall.hideFlags = HideFlags.NotEditable;
                     }
-                    else if(tileID == FloorType.VerticalEdgeWall && x == 0)
+                    else if (tileID == FloorType.VerticalWall && x == 0)
                     {
-                        Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.identity, this.transform);
+                        spawnedWall = Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.identity, boundaryHolder);
+                        spawnedWall.hideFlags = HideFlags.NotEditable;
                     }
-                    else if(tileID == FloorType.VerticalEdgeWall && x == width -1)
+                    else if (tileID == FloorType.VerticalWall && x == width - 1)
                     {
-                        Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.Euler(new Vector3(0.0f, 180.0f, 0.0f)), this.transform);
+                        spawnedWall = Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.Euler(new Vector3(0.0f, 180.0f, 0.0f)), boundaryHolder);
+                        spawnedWall.hideFlags = HideFlags.NotEditable;
                     }
-                    else if(tileID == FloorType.EdgeWall && (x == width - 1 && y == 0))
+                    else if (tileID == FloorType.BaseCornerWall && (x == width - 1 && y == 0))
                     {
-                        Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.Euler(new Vector3(0.0f, 180.0f, 0.0f)), this.transform);
+                        spawnedWall = Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.Euler(new Vector3(0.0f, 180.0f, 0.0f)), boundaryHolder);
+                        spawnedWall.hideFlags = HideFlags.NotEditable;
                     }
-                    else if(tileID == FloorType.EdgeWall && (x == 0 && y == height - 1))
+                    else if (tileID == FloorType.BaseCornerWall && (x == 0 && y == height - 1))
                     {
-                        Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.Euler(new Vector3(180.0f, 0.0f, 0.0f)), this.transform);
+                        spawnedWall = Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.Euler(new Vector3(180.0f, 0.0f, 0.0f)), boundaryHolder);
+                        spawnedWall.hideFlags = HideFlags.NotEditable;
                     }
-                    else if(tileID == FloorType.EdgeWall && (x == width - 1 && y == height - 1))
+                    else if (tileID == FloorType.BaseCornerWall && (x == width - 1 && y == height - 1))
                     {
-                        Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.Euler(new Vector3(180.0f, 180.0f, 0.0f)), this.transform);
+                        spawnedWall = Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.Euler(new Vector3(180.0f, 180.0f, 0.0f)), boundaryHolder);
+                        spawnedWall.hideFlags = HideFlags.NotEditable;
                     }
                     else
                     {
-                        // Instantiate the prefab at the calculated position
-                        Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.identity, this.transform);
-                    }
-
-                }
-                else
-                {
-                    Debug.LogWarning($"No prefab assigned for tile ID: {tileID}");
+                        spawnedWall = Instantiate(tilePrefabs[((int)tileID)], spawnPosition, Quaternion.identity, boundaryHolder);
+                        spawnedWall.hideFlags = HideFlags.None;
+                    }   
                 }
             }
+        }
+    }
+
+    void ClearOldBoundaries()
+    {
+        // Find the specific folder holding our generated walls and destroy it
+        Transform oldHolder = transform.Find("GeneratedBoundaries");
+        if (oldHolder != null)
+        {
+            DestroyImmediate(oldHolder.gameObject);
         }
     }
 }
