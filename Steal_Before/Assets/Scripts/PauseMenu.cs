6e5a9217c6c7129eed7622f4fc38c2966724bd20
Raffong <raphaelce.lim@gmail.com>
2026-04-16 14:36:09 +0800

    credits added, pause menu button got prob

--- Steal_Before/Assets/Scripts/PauseMenu.cs
+++ Steal_Before/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.UI; // Required if you want to change the button icon
+
+//Audio
+public class VolumeControl : MonoBehaviour
+{
+    private bool isMuted = false;
+
+    public void ToggleMute()
+    {
+        isMuted = !isMuted;
+
+        // This silences all AudioSources in the entire game
+        AudioListener.pause = isMuted;
+
+        // Debug to verify in console
+        Debug.Log(isMuted ? "Game Muted" : "Game Unmuted");
+    }
+}
 
 public class PauseMenu : MonoBehaviour
 {
